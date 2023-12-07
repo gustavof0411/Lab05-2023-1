f2b94bdff040c0d237205667ef6606b1a92e54fa
gustavof0411 <136523482+gustavof0411@users.noreply.github.com>
2023-12-07 20:47:19 +0000

    Merge funcional

--- src/main/ordenadores/OrdenarPorMerge.java
+++ src/main/ordenadores/OrdenarPorMerge.java
@@ -7,7 +7,21 @@ public class OrdenarPorMerge implements Ordenador{
     }
 
     public int[] merge(int[] array, int[] array2) {
-        return array;
-    }
+        int seta1 = 0;
+        int seta2 = 0;
+        int[] arrayMerge = new int[array.length + array2.length];
     
+        for (int i = 0; i < arrayMerge.length; i++) {
+            if (seta1 < array.length && (seta2 >= array2.length || array[seta1] < array2[seta2])) {
+                arrayMerge[i] = array[seta1];
+                seta1++;
+            } else {
+                arrayMerge[i] = array2[seta2];
+                seta2++;
+            }
+        }
+    
+        return arrayMerge;
+    }
 }
+    
