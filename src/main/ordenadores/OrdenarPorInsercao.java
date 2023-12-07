ff69b50935d5cf7ccc4f8b3a5cf91e9dc9124c2c
gustavof0411 <136523482+gustavof0411@users.noreply.github.com>
2023-12-07 20:38:31 +0000

    Inserção e Seleção funcionais

--- src/main/ordenadores/OrdenarPorInsercao.java
+++ src/main/ordenadores/OrdenarPorInsercao.java
@@ -1,10 +1,20 @@
 package ordenadores;
-public class OrdenarPorInsercao implements Ordenador{
+
+public class OrdenarPorInsercao implements Ordenador {
 
     @Override
     public int[] ordene(int[] array) {
-
+        if (array != null) {
+            for (int i = 1; i < array.length; i++) {
+                int salva = array[i];
+                int proximo = i - 1;
+                while (proximo >= 0 && array[proximo] > salva) {
+                    array[proximo + 1] = array[proximo];
+                    proximo = proximo - 1;
+                }
+                array[proximo + 1] = salva;
+            }
+        }
         return array;
     }
-    
 }
--- src/main/ordenadores/OrdenarPorSelecao.java
+++ src/main/ordenadores/OrdenarPorSelecao.java
@@ -1,11 +1,22 @@
 package ordenadores;
 
-public class OrdenarPorSelecao implements Ordenador{
+public class OrdenarPorSelecao implements Ordenador {
 
     @Override
     public int[] ordene(int[] array) {
-
+        if (array != null && array.length > 1) {
+            for (int i = 0; i < array.length - 1; i++) {
+                int indiceMenor = i;
+                for (int proximo = i + 1; proximo < array.length; proximo++) {
+                    if (array[proximo] < array[indiceMenor]) {
+                        indiceMenor = proximo;
+                    }
+                }
+                int salva = array[indiceMenor];
+                array[indiceMenor] = array[i];
+                array[i] = salva;
+            }
+        }
         return array;
     }
-    
 }
